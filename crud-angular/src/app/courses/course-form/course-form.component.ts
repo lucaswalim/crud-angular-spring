3a961c7fcfc3e10c10c1ff90b707eb47b06f64a2
Lucas Walim da Silva <lucaswalim@hotmail.com>
2024-03-25 19:40:17 -0300

    Atualização Angular cli e core de 13 para 14

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Location } from '@angular/common';
@@ -10,10 +10,10 @@ import { Location } from '@angular/common';
   styleUrls: ['./course-form.component.css'],
 })
 export class CourseFormComponent implements OnInit {
-  form: FormGroup;
+  form: UntypedFormGroup;
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: UntypedFormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
     private location: Location
